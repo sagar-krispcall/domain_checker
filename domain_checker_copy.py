87f3c2d5051e4def86b6930312a3f6ddfbf54b92
sagar-krispcall <sagar.upadhyaya@krispcallmail.com>
2025-11-11 09:55:48 +0545

    changes

--- domain_checker_copy.py
+++ domain_checker_copy.py
@@ -15,17 +15,17 @@ from bs4 import BeautifulSoup
 # =====================================================
 load_dotenv()
 api_keys = [
-    os.getenv("GEMINI_API_KEY_1"),
-    os.getenv("GEMINI_API_KEY_2"),
-    os.getenv("GEMINI_API_KEY_3")
-    #os.getenv("GEMINI_API_KEY_4"),
-    #os.getenv("GEMINI_API_KEY_5"),
-    #os.getenv("GEMINI_API_KEY_6"),
-    #os.getenv("GEMINI_API_KEY_7"),
-    #os.getenv("GEMINI_API_KEY_8"),
-    #os.getenv("GEMINI_API_KEY_9"),
-    #os.getenv("GEMINI_API_KEY_10"),
-    #os.getenv("GEMINI_API_KEY_11")
+    # os.getenv("GEMINI_API_KEY_1"),
+    # os.getenv("GEMINI_API_KEY_2"),
+    # os.getenv("GEMINI_API_KEY_3")
+    # os.getenv("GEMINI_API_KEY_4"),
+    # os.getenv("GEMINI_API_KEY_5"),
+    # os.getenv("GEMINI_API_KEY_6"),
+    os.getenv("GEMINI_API_KEY_7")
+    # os.getenv("GEMINI_API_KEY_8"),
+    # os.getenv("GEMINI_API_KEY_9"),
+    # os.getenv("GEMINI_API_KEY_10"),
+    # os.getenv("GEMINI_API_KEY_11")
 ]
 
 api_keys = [k for k in api_keys if k]
